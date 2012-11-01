e0497e5ddca50b7280d9fc66070b05f4d82b597b
kevin birch <kmb@pobox.com>
2012-11-01 17:03:10 -0400

    add license file; update license heaeers

--- src/main/c/include/bash.h
+++ src/main/c/include/bash.h
@@ -1,3 +1,34 @@
+/*
+ * 金棒 (kanabō)
+ * Copyright (c) 2012 Kevin Birch <kmb@pobox.com>
+ * 
+ * 金棒 is a tool to bludgeon YAML and JSON files from the shell: the strong
+ * made stronger.
+ *
+ * For more information, consult the README in the project root.
+ *
+ * Distributed under an [MIT-style] license.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * [MIT-style]: http://www.opensource.org/licenses/mit-license.php
+ */
 
 #ifndef BASH_H
 #define BASH_H
