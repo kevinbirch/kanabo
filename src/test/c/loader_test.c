9593918cb5ab0bf0f8b7a9931dd6dbcd8475a624
kevin birch <kmb@pobox.com>
2012-12-03 19:47:14 -0500

    simplify tests

--- src/test/c/loader_test.c
+++ src/test/c/loader_test.c
@@ -104,51 +104,41 @@ static void assert_model_state(int result, document_model *model)
     ck_assert_int_eq(5, node_get_size(root));
     ck_assert_not_null(mapping_get_all(root));
 
-    key_value_pair *one = mapping_get_key_value(root, 0);
+    node *one = mapping_get_value(root, "one");
     ck_assert_not_null(one);
-    ck_assert_int_eq(SCALAR, node_get_kind(one->key));
-    ck_assert_buf_eq("one", 3, scalar_get_value(one->key), node_get_size(one->key));
-    ck_assert_int_eq(SEQUENCE, node_get_kind(one->value));
-    ck_assert_int_eq(2, node_get_size(one->value));
-    node *s0_0 = sequence_get_item(one->value, 0);
-    ck_assert_int_eq(SCALAR, node_get_kind(s0_0));
-    ck_assert_buf_eq("foo1", 4, scalar_get_value(s0_0), node_get_size(s0_0));
-    node *s0_1 = sequence_get_item(one->value, 1);
-    ck_assert_int_eq(SCALAR, node_get_kind(s0_1));
-    ck_assert_buf_eq("bar1", 4, scalar_get_value(s0_1), node_get_size(s0_1));
+    ck_assert_int_eq(SEQUENCE, node_get_kind(one));
+    ck_assert_int_eq(2, node_get_size(one));
+    node *one_0 = sequence_get_item(one, 0);
+    ck_assert_int_eq(SCALAR, node_get_kind(one_0));
+    ck_assert_buf_eq("foo1", 4, scalar_get_value(one_0), node_get_size(one_0));
+    node *one_1 = sequence_get_item(one, 1);
+    ck_assert_int_eq(SCALAR, node_get_kind(one_1));
+    ck_assert_buf_eq("bar1", 4, scalar_get_value(one_1), node_get_size(one_1));
 
-    key_value_pair *two = mapping_get_key_value(root, 1);
+    node *two = mapping_get_value(root, "two");
     ck_assert_not_null(two);
-    ck_assert_int_eq(SCALAR, node_get_kind(two->key));
-    ck_assert_buf_eq("two", 3, scalar_get_value(two->key), node_get_size(two->key));
-    ck_assert_int_eq(SCALAR, node_get_kind(two->value));
-    ck_assert_buf_eq("foo2", 4, scalar_get_value(two->value), node_get_size(two->value));
+    ck_assert_int_eq(SCALAR, node_get_kind(two));
+    ck_assert_buf_eq("foo2", 4, scalar_get_value(two), node_get_size(two));
 
-    key_value_pair *three = mapping_get_key_value(root, 2);
+    node *three = mapping_get_value(root, "three");
     ck_assert_not_null(three);
-    ck_assert_int_eq(SCALAR, node_get_kind(three->key));
-    ck_assert_buf_eq("three", 5, scalar_get_value(three->key), node_get_size(three->key));
-    ck_assert_int_eq(SCALAR, node_get_kind(three->value));
-    ck_assert_buf_eq("foo3", 4, scalar_get_value(three->value), node_get_size(three->value));
+    ck_assert_int_eq(SCALAR, node_get_kind(three));
+    ck_assert_buf_eq("foo3", 4, scalar_get_value(three), node_get_size(three));
 
-    key_value_pair *four = mapping_get_key_value(root, 3);
+    node *four = mapping_get_value(root, "four");
     ck_assert_not_null(four);
-    ck_assert_int_eq(SCALAR, node_get_kind(four->key));
-    ck_assert_buf_eq("four", 4, scalar_get_value(four->key), node_get_size(four->key));
-    ck_assert_int_eq(SEQUENCE, node_get_kind(four->value));
-    node *s3_0 = sequence_get_item(four->value, 0);
-    ck_assert_int_eq(SCALAR, node_get_kind(s3_0));
-    ck_assert_buf_eq("foo4", 4, scalar_get_value(s3_0), node_get_size(s3_0));
-    node *s3_1 = sequence_get_item(four->value, 1);
-    ck_assert_int_eq(SCALAR, node_get_kind(s3_0));
-    ck_assert_buf_eq("bar4", 4, scalar_get_value(s3_1), node_get_size(s3_1));
+    ck_assert_int_eq(SEQUENCE, node_get_kind(four));
+    node *four_0 = sequence_get_item(four, 0);
+    ck_assert_int_eq(SCALAR, node_get_kind(four_0));
+    ck_assert_buf_eq("foo4", 4, scalar_get_value(four_0), node_get_size(four_0));
+    node *four_1 = sequence_get_item(four, 1);
+    ck_assert_int_eq(SCALAR, node_get_kind(four_0));
+    ck_assert_buf_eq("bar4", 4, scalar_get_value(four_1), node_get_size(four_1));
 
-    key_value_pair *five = mapping_get_key_value(root, 4);
+    node *five = mapping_get_value(root, "five");
     ck_assert_not_null(five);
-    ck_assert_int_eq(SCALAR, node_get_kind(five->key));
-    ck_assert_buf_eq("five", 4, scalar_get_value(five->key), node_get_size(five->key));
-    ck_assert_int_eq(SCALAR, node_get_kind(five->value));
-    ck_assert_buf_eq("foo5", 4, scalar_get_value(five->value), node_get_size(five->value));
+    ck_assert_int_eq(SCALAR, node_get_kind(five));
+    ck_assert_buf_eq("foo5", 4, scalar_get_value(five), node_get_size(five));
 }
 
 Suite *loader_suite(void)
