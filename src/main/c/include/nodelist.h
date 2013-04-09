caafa841b91e5f0b3ec7df442a248c3e5c1b70a0
kevin birch <kmb@pobox.com>
2013-04-09 00:50:54 -0400

    support add all

--- src/main/c/include/nodelist.h
+++ src/main/c/include/nodelist.h
@@ -64,16 +64,23 @@ bool   nodelist_clear(nodelist *list);
 size_t nodelist_length(const nodelist * restrict list);
 bool   nodelist_is_empty(const nodelist * restrict list);
 node  *nodelist_get(const nodelist * restrict list, size_t index);
-bool   nodelist_add(nodelist * restrict list,  node *value);
+bool   nodelist_add(nodelist * restrict list,  node * restrict value);
+bool   nodelist_add_all(nodelist * restrict list,  nodelist * restrict value);
 bool   nodelist_set(nodelist * restrict list, node *value, size_t index);
 
 typedef bool (*nodelist_iterator)(node *each, void *context);
 bool nodelist_iterate(const nodelist * restrict list, nodelist_iterator iterator, void *context);
 
-typedef node *(*nodelist_function)(node *each, void *context);
-nodelist *nodelist_map(const nodelist * restrict list, nodelist_function function, void *context);
-nodelist *nodelist_map_into(const nodelist * restrict list, nodelist_function function, void *context, nodelist * restrict target);
-nodelist *nodelist_map_overwrite(const nodelist * restrict list, nodelist_function function, void *context, nodelist * restrict target);
+bool add_to_nodelist_iterator(node *each, void *context);
+
+typedef node *(*nodelist_to_one_function)(node *each, void *context);
+nodelist *nodelist_map(const nodelist * restrict list, nodelist_to_one_function function, void *context);
+nodelist *nodelist_map_into(const nodelist * restrict list, nodelist_to_one_function function, void *context, nodelist * restrict target);
+nodelist *nodelist_map_overwrite(const nodelist * restrict list, nodelist_to_one_function function, void *context, nodelist * restrict target);
+
+typedef nodelist *(*nodelist_to_many_function)(node *each, void *context);
+nodelist *nodelist_flatmap(const nodelist * restrict list, nodelist_to_many_function function, void *context);
+nodelist *nodelist_flatmap_into(const nodelist * restrict list, nodelist_to_many_function function, void *context, nodelist * restrict target);
 
 
 #endif
