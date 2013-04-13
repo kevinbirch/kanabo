34e8d1b57f699c888e1e84103dc2fa927a19e284
kevin birch <kmb@pobox.com>
2013-04-12 22:50:01 -0400

    add stdio back to test header

--- src/test/c/include/test.h
+++ src/test/c/include/test.h
@@ -38,6 +38,8 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include <stdio.h>
+
 #define ck_assert_null(X) ck_assert_msg((X) == NULL, "Assertion '"#X"==NULL' failed")
 #define ck_assert_not_null(X) ck_assert_msg((X) != NULL, "Assertion '"#X"!=NULL' failed")
 #define ck_assert_buf_eq(X, N1, Y, N2) ck_assert_msg(memcmp((X), (Y), (N1) > (N2) ? (N2) : (N1)) == 0, "Assertion 'memcmp("#X", "#Y", %zd)' failed", (N1) > (N2) ? (N2) : (N1))
