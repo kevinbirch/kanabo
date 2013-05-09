60a66373588d6b0bb00e47b5bc767e3f71b1a8b2
kevin birch <kmb@pobox.com>
2013-05-08 20:34:07 -0400

    version 0.1.1-snapshot

--- src/main/brew/kanabo.rb
+++ src/main/brew/kanabo.rb
@@ -2,8 +2,7 @@ require 'formula'
 
 class Kanabo < Formula
   homepage 'https://github.com/kevinbirch/kanabo'
-  url 'https://github.com/kevinbirch/kanabo/archive/kanabo-0.1.0-SNAPSHOT.tar.gz'
-  sha1 '4ab9e6696b760dd3abe834cbe36e77235629c7a8'
+  url 'https://github.com/kevinbirch/kanabo/archive/kanabo-0.1.1-SNAPSHOT.tar.gz'
 
   depends_on 'check' => :build
   depends_on 'libyaml'
