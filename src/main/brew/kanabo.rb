e35acbaa04f3fa957bf93dc49e73f116765cda23
kevin birch <kmb@pobox.com>
2013-05-08 20:35:50 -0400

    add sha1

--- src/main/brew/kanabo.rb
+++ src/main/brew/kanabo.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Kanabo < Formula
   homepage 'https://github.com/kevinbirch/kanabo'
   url 'https://github.com/kevinbirch/kanabo/archive/kanabo-0.1.1-SNAPSHOT.tar.gz'
+  sha1 '483a6db0baaf5dc4baf5c8d2a056b4dd08e21412'
 
   depends_on 'check' => :build
   depends_on 'libyaml'
