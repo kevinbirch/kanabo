94e4e98fb7cc1adc6244044305508a03aa751a59
kevin birch <kmb@pobox.com>
2013-12-03 19:56:17 -0500

    update vector code

--- src/main/c/include/vector.h
+++ src/main/c/include/vector.h
@@ -36,31 +36,68 @@
 
 typedef struct vector_s Vector;
 
-typedef bool (*vector_iterator)(void *each, void *context);
-typedef bool (*vector_map_function)(void *each, void *context, Vector *target);
-typedef bool (*vector_item_comparitor)(const void *one, const void *two);
+/* Callback Functions */
+typedef bool  (*vector_iterator)(void *each, void *context);
+typedef bool  (*vector_mapper)(void *each, void *context, Vector *target);
+typedef bool  (*vector_comparitor)(const void *one, const void *two);
+typedef void *(*vector_reducer)(const void *one, const void *two, void *context);
 
+/* Constructors */
 Vector *make_vector(void);
 Vector *make_vector_with_capacity(size_t capacity);
+Vector *make_vector_of(size_t count, ...);
+Vector *vector_copy(const Vector *vector);
+Vector *vector_with(const Vector *vector, void *value);
+Vector *vector_with_all(const Vector *vector, const Vector *from);
 
+/* Destructor */
 void    vector_free(Vector *value);
-        
+
+/* Size API */
 size_t  vector_length(const Vector *vector);
 size_t  vector_capacity(const Vector *vector);
 bool    vector_is_empty(const Vector *vector);
-        
-void   *vector_get(const Vector *vector, size_t index);
-bool    vector_add(Vector *vector,  void *value);
-bool    vector_add_all(Vector *vector,  Vector *value);
-void   *vector_set(Vector *vector, void *value, size_t index);
-void   *vector_remove(Vector *vector, size_t index);
 
+/* Element API */
+void   *vector_get(const Vector *vector, size_t index);
+void   *vector_first(const Vector *vector);
+#define vector_head vector_first
+void   *vector_last(const Vector *vector);
+
+/* Mutation API */
+bool    vector_add(Vector *vector, void *value);
+#define vector_append vector_add
+bool    vector_add_all(Vector *vector, const Vector *value);
+bool    vector_insert(Vector *vector, void *value, size_t index);
+#define vector_prepend(VECTOR, VALUE) vector_insert((VECTOR), (VALUE), 0)
+void   *vector_set(Vector *vector, void *value, size_t index);
+
+/* Deletion API */
+void   *vector_remove(Vector *vector, size_t index);
+bool    vector_remove_item(Vector *vector, vector_comparitor comparitor, void *item);
 void    vector_clear(Vector *vector);
 bool    vector_trim(Vector *vector);
-        
-bool    vector_equals(const Vector *one, const Vector *two, vector_item_comparitor comparitor);
+
+/* Queue API */
+#define vector_peek(VECTOR) vector_get((VECTOR), vector_length((VECTOR)) - 1)
+#define vector_push vector_add
+#define vector_pop(VECTOR) vector_remove((VECTOR), vector_length((VECTOR)) - 1)
+
+/* Search API */
+void   *vector_find(const Vector *vector, vector_iterator iterator, void *context);
+bool    vector_contains(const Vector *vector, vector_comparitor comparitor, void *item);
+bool    vector_any(const Vector *vector, vector_iterator iterator, void *context);
+bool    vector_all(const Vector *vector, vector_iterator iterator, void *context);
+bool    vector_none(const Vector *vector, vector_iterator iterator, void *context);
+size_t  vector_count(const Vector *vector, vector_iterator iterator, void *context);
+
+/* Comparison API */
+bool    vector_equals(const Vector *one, const Vector *two, vector_comparitor comparitor);
+
+/* Functional API */
 bool    vector_iterate(const Vector *vector, vector_iterator iterator, void *context);
-
-Vector *vector_map(const Vector *vector, vector_map_function function, void *context);
-Vector *vector_map_into(const Vector *vector, vector_map_function function, void *context, Vector *target);
-
+Vector *vector_map(const Vector *vector, vector_mapper function, void *context);
+Vector *vector_map_into(const Vector *vector, vector_mapper function, void *context, Vector *target);
+void   *vector_reduce(const Vector *vector, vector_reducer function, void *context);
+Vector *vector_filter(const Vector *vector, vector_iterator function, void *context);
+Vector *vector_filter_not(const Vector *vector, vector_iterator function, void *context);
--- src/main/c/util/vector.c
+++ src/main/c/util/vector.c
@@ -30,8 +30,9 @@
  */
 
 #include <stdint.h>
-#include <errno.h>
+#include <stdarg.h>
 #include <string.h>
+#include <errno.h>
 
 #include "vector.h"
 
@@ -45,6 +46,9 @@ struct vector_s
 };
 
 static inline bool ensure_capacity(Vector *vector, size_t min_capacity);
+static inline size_t calculate_new_capacity(size_t capacity);
+static inline bool reallocate(Vector *vector, size_t capacity);
+
 
 static bool add_to_vector_iterator(void *each, void *context);
 
@@ -55,6 +59,11 @@ Vector *make_vector(void)
 
 Vector *make_vector_with_capacity(size_t capacity)
 {
+    if(0 == capacity)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
     Vector *result = (Vector *)calloc(1, sizeof(Vector));
     if(NULL == result)
     {
@@ -70,6 +79,84 @@ Vector *make_vector_with_capacity(size_t capacity)
 
     result->length = 0;
     result->capacity = capacity;
+    return result;
+}
+
+Vector *make_vector_of(size_t count, ...)
+{
+    Vector *result = make_vector_with_capacity(count);
+    if(NULL == result)
+    {
+        return NULL;
+    }
+
+    va_list rest;
+    va_start(rest, count);
+    for(size_t i = 0; i < count; i++)
+    {
+        void *element = va_arg(rest, void *);
+        result->items[i] = element;
+    }
+    va_end(rest);
+    result->length = count;
+
+    return result;
+}
+
+Vector *vector_copy(const Vector *vector)
+{
+    if(NULL == vector)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    Vector *result = make_vector_with_capacity(vector->length);
+    if(NULL == result)
+    {
+        return NULL;
+    }
+    memcpy(result->items, vector->items, sizeof(uint8_t *) * vector->length);
+    result->length = vector->length;
+
+    return result;
+}
+
+Vector *vector_with(const Vector *vector, void *value)
+{
+    if(NULL == vector || NULL == value)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    Vector *result = vector_copy(vector);
+    if(NULL == result)
+    {
+        return NULL;
+    }
+
+    vector_add(result, value);
+
+    return result;
+}
+
+Vector *vector_with_all(const Vector *vector, const Vector *from)
+{
+    if(NULL == vector || NULL == from)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    Vector *result = vector_copy(vector);
+    if(NULL == result)
+    {
+        return NULL;
+    }
+
+    vector_add_all(result, from);
+
     return result;
 }
 
@@ -132,6 +219,16 @@ void *vector_get(const Vector *vector, size_t index)
     return vector->items[index];
 }
 
+void *vector_first(const Vector *vector)
+{
+    return vector_get(vector, 0);
+}
+
+void   *vector_last(const Vector *vector)
+{
+    return vector_get(vector, vector->length - 1);
+}
+
 bool vector_add(Vector *vector, void *value)
 {
     if(NULL == vector || NULL == value)
@@ -154,20 +251,71 @@ static bool add_to_vector_iterator(void *each, void *context)
     return vector_add(vector, each);
 }
 
-bool vector_add_all(Vector *vector, Vector *value)
+bool vector_add_all(Vector *vector, const Vector *from)
 {
-    if(NULL == vector || NULL == value)
+    if(NULL == vector || NULL == from)
     {
         errno = EINVAL;
         return false;
     }
 
-    if(!ensure_capacity(vector, vector->length + value->length))
+    if(!ensure_capacity(vector, vector->length + from->length))
     {
         return false;
     }
-    bool result = vector_iterate(value, add_to_vector_iterator, vector);
-    return result;
+
+    return vector_iterate(from, add_to_vector_iterator, vector);
+}
+
+bool vector_insert(Vector *vector, void *value, size_t index)
+{
+    if(NULL == vector || NULL == value || index > vector->length)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    if(vector->length == index)
+    {
+        return vector_add(vector, value);
+    }
+
+    uint8_t **target = vector->items;
+    if(vector->capacity < vector->length + 1)
+    {
+        size_t new_capacity = calculate_new_capacity(vector->capacity);
+        target = calloc(1, sizeof(uint8_t *) * new_capacity);
+        if(NULL == vector->items)
+        {
+            return false;
+        }
+        vector->capacity = new_capacity;
+    }
+
+    if(0 == index)
+    {
+        memmove(target + 1, vector->items, sizeof(uint8_t *) * vector->length);
+        target[0] = value;
+    }
+    else
+    {
+        if(target != vector->items)
+        {
+            memcpy(target, vector->items, sizeof(uint8_t *) * index);
+        }
+        memmove(target + index + 1, vector->items + index, 
+                sizeof(uint8_t *) * (vector->length - index));
+        target[index] = value;
+    }
+
+    if(target != vector->items)
+    {
+        free(vector->items);
+        vector->items = target;
+    }
+    vector->length++;
+
+    return true;
 }
 
 void *vector_set(Vector *vector, void *value, size_t index)
@@ -198,11 +346,29 @@ void *vector_remove(Vector *vector, size_t index)
     }
     else
     {
-        memcpy(vector->items + index, vector->items + index + 1, sizeof(uint8_t *) * (vector->length - (index + 1)));
+        memmove(vector->items + index, vector->items + index + 1, sizeof(uint8_t *) * (vector->length - (index + 1)));
         vector->items[vector->length - 1] = NULL;
     }
     vector->length--;
     return previous;
+}
+
+bool vector_remove_item(Vector *vector, vector_comparitor comparitor, void *item)
+{
+    if(NULL == vector || NULL == comparitor)
+    {
+        errno = EINVAL;
+        return false;
+    }
+
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(comparitor(vector->items[i], item))
+        {
+            return NULL != vector_remove(vector, i);
+        }
+    }
+    return false;
 }
 
 void vector_clear(Vector *vector)
@@ -225,19 +391,120 @@ bool vector_trim(Vector *vector)
     {
         return false;
     }
+    
+    return reallocate(vector, vector->length);
+}
 
-    uint8_t **cache = vector->items;
-    vector->items = realloc(vector->items, sizeof(void *) * vector->length);
-    if(NULL == vector->items)
+void *vector_find(const Vector *vector, vector_iterator iterator, void *context)
+{
+    if(NULL == vector || NULL == iterator)
     {
-        vector->items = cache;
+        errno = EINVAL;
+        return NULL;
+    }
+
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(iterator(vector->items[i], context))
+        {
+            return vector->items[i];
+        }
+    }
+    return NULL;
+}
+
+bool vector_contains(const Vector *vector, vector_comparitor comparitor, void *item)
+{
+    if(NULL == vector || NULL == comparitor)
+    {
+        errno = EINVAL;
         return false;
     }
-    vector->capacity = vector->length;
+
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(comparitor(vector->items[i], item))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool vector_any(const Vector *vector, vector_iterator iterator, void *context)
+{
+    if(NULL == vector || NULL == iterator)
+    {
+        errno = EINVAL;
+        return false;
+    }
+
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(iterator(vector->items[i], context))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool vector_all(const Vector *vector, vector_iterator iterator, void *context)
+{
+    if(NULL == vector || NULL == iterator)
+    {
+        errno = EINVAL;
+        return false;
+    }
+
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(!iterator(vector->items[i], context))
+        {
+            return false;
+        }
+    }
     return true;
 }
 
-bool vector_equals(const Vector *one, const Vector *two, vector_item_comparitor comparitor)
+bool vector_none(const Vector *vector, vector_iterator iterator, void *context)
+{
+    if(NULL == vector || NULL == iterator)
+    {
+        errno = EINVAL;
+        return false;
+    }
+
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(iterator(vector->items[i], context))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+size_t vector_count(const Vector *vector, vector_iterator iterator, void *context)
+{
+    if(NULL == vector || NULL == iterator)
+    {
+        errno = EINVAL;
+        return 0;
+    }
+
+    size_t count = 0;
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(iterator(vector->items[i], context))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+bool vector_equals(const Vector *one, const Vector *two, vector_comparitor comparitor)
 {
     if(one == two)
     {
@@ -265,7 +532,7 @@ bool vector_iterate(const Vector *vector, vector_iterator iterator, void *contex
         return false;
     }
 
-    for(size_t i = 0; i < vector_length(vector); i++)
+    for(size_t i = 0; i < vector->length; i++)
     {
         if(!iterator(vector->items[i], context))
         {
@@ -275,7 +542,7 @@ bool vector_iterate(const Vector *vector, vector_iterator iterator, void *contex
     return true;
 }
 
-Vector *vector_map(const Vector *vector, vector_map_function function, void *context)
+Vector *vector_map(const Vector *vector, vector_mapper function, void *context)
 {
     if(NULL == vector || NULL == function)
     {
@@ -283,7 +550,7 @@ Vector *vector_map(const Vector *vector, vector_map_function function, void *con
         return false;
     }
 
-    Vector *target = make_vector_with_capacity(vector_length(vector));
+    Vector *target = make_vector_with_capacity(vector->length);
     if(NULL == target)
     {
         return NULL;
@@ -298,7 +565,7 @@ Vector *vector_map(const Vector *vector, vector_map_function function, void *con
     return target;            
 }
 
-Vector *vector_map_into(const Vector *vector, vector_map_function function, void *context, Vector *target)
+Vector *vector_map_into(const Vector *vector, vector_mapper function, void *context, Vector *target)
 {
     if(NULL == vector || NULL == function || NULL == target)
     {
@@ -306,7 +573,7 @@ Vector *vector_map_into(const Vector *vector, vector_map_function function, void
         return false;
     }
 
-    for(size_t i = 0; i < vector_length(vector); i++)
+    for(size_t i = 0; i < vector->length; i++)
     {
         if(!function(vector->items[i], context, target))
         {
@@ -317,21 +584,110 @@ Vector *vector_map_into(const Vector *vector, vector_map_function function, void
     return target;
 }
 
+void *vector_reduce(const Vector *vector, vector_reducer function, void *context)
+{
+    if(NULL == vector || NULL == function || vector_is_empty(vector))
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    if(1 == vector->length)
+    {
+        return vector->items[0];
+    }
+
+    void *result = function(vector->items[0], vector->items[1], context);
+    for(size_t i = 2; i < vector->length; i++)
+    {
+        result = function(result, vector->items[i], context);
+    }
+
+    return result;
+}
+
+Vector *vector_filter(const Vector *vector, vector_iterator function, void *context)
+{
+    if(NULL == vector || NULL == function)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    if(vector_is_empty(vector))
+    {
+        return make_vector();
+    }
+
+    Vector *result = make_vector_with_capacity(vector->length);
+    if(NULL == result)
+    {
+        return NULL;
+    }
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(function(vector->items[i], context))
+        {
+            result->items[result->length++] = vector->items[i];
+        }
+    }
+
+    return result;
+}
+
+Vector *vector_filter_not(const Vector *vector, vector_iterator function, void *context)
+{
+    if(NULL == vector || NULL == function)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    if(vector_is_empty(vector))
+    {
+        return make_vector();
+    }
+
+    Vector *result = make_vector_with_capacity(vector->length);
+    if(NULL == result)
+    {
+        return NULL;
+    }
+    for(size_t i = 0; i < vector->length; i++)
+    {
+        if(!function(vector->items[i], context))
+        {
+            result->items[result->length++] = vector->items[i];
+        }
+    }
+
+    return result;
+}
+
+static inline size_t calculate_new_capacity(size_t capacity)
+{
+    return (capacity * 3) / 2 + 1;
+}
+
 static inline bool ensure_capacity(Vector *vector, size_t min_capacity)
 {
     if(vector->capacity < min_capacity)
     {
-        size_t new_capacity = (min_capacity * 3) / 2 + 1;
-        uint8_t **cache = vector->items;
-        vector->items = realloc(vector->items, sizeof(void *) * new_capacity);
-        if(NULL == vector->items)
-        {
-            vector->items = cache;
-            return false;
-        }
-        vector->capacity = new_capacity;
+        size_t new_capacity = calculate_new_capacity(min_capacity);
+        return reallocate(vector, new_capacity);
     }
     return true;
 }
 
-
+static inline bool reallocate(Vector *vector, size_t capacity)
+{
+    uint8_t **cache = vector->items;
+    vector->items = realloc(vector->items, sizeof(uint8_t *) * capacity);
+    if(NULL == vector->items)
+    {
+        vector->items = cache;
+        return false;
+    }
+    vector->capacity = capacity;
+    return true;
+}
