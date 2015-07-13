17d754aad2c53d051ecd695cede7179eb29cc3af
kevin birch <kmb@percolate.com>
2015-07-12 21:10:53 -0400

    add parser delegate declarations

--- src/main/c/include/jsonpath/combinators/base.h
+++ src/main/c/include/jsonpath/combinators/base.h
@@ -35,9 +35,12 @@
  * [license]: http://www.opensource.org/licenses/ncsa
  */
 
+
 #pragma once
 
 
+#include "jsonpath/ast.h"
+#include "jsonpath/input.h"
 #include "jsonpath/combinators.h"
 #include "jsonpath/logging.h"
 
@@ -58,6 +61,52 @@ enum parser_kind
     STRING
 };
 
+enum parser_result_code
+{
+    PARSER_SUCCESS = 0,
+    ERR_NULL_EXPRESSION,             // the expression argument given was NULL
+    ERR_ZERO_LENGTH,                 // expression length was 0
+    ERR_PARSER_OUT_OF_MEMORY,        // unable to allocate memory
+    ERR_NOT_JSONPATH,                // not a JSONPath expression
+    ERR_PREMATURE_END_OF_INPUT,      // premature end of input before expression was complete
+    ERR_UNEXPECTED_VALUE,            // expected one character but found another
+    ERR_EMPTY_PREDICATE,             // a predicate is empty
+    ERR_UNBALANCED_PRED_DELIM,       // missing closing predicate delimiter `]' before end of step
+    ERR_UNSUPPORTED_PRED_TYPE,       // unsupported predicate found
+    ERR_EXTRA_JUNK_AFTER_PREDICATE,  // extra characters after valid predicate definition
+    ERR_EXPECTED_NAME_CHAR,          // expected a name character, but found something else instead
+    ERR_EXPECTED_NODE_TYPE_TEST,     // expected a node type test
+    ERR_EXPECTED_INTEGER,            // expected an integer
+    ERR_INVALID_NUMBER,              // invalid number
+    ERR_STEP_CANNOT_BE_ZERO,         // slice step value must be non-zero
+    ERR_NO_ALTERNATIVE,              // none of the alternatives matched the input
+};
+
+typedef enum parser_result_code parser_result_code;
+
+struct maybe_ast_s
+{
+    enum
+    {
+        AST_ERROR,
+        AST_VALUE
+    } tag;
+
+    union
+    {
+        struct
+        {
+            parser_result_code  code;
+            uint8_t argument;
+        } error;
+        Ast *value;
+    };
+};
+
+typedef struct maybe_ast_s MaybeAst;
+
+typedef MaybeAst (*parser_function)(MaybeAst ast, Input *input);
+
 struct vtable_s
 {
     void (*free)(Parser *self);
@@ -71,6 +120,15 @@ struct parser_s
     parser_function        function;
 };
 
+
+#define collection() (MaybeAst){AST_VALUE, .value=make_ast_node(AST_COLLECTION, NULL)}
+#define error(CODE) (MaybeAst){AST_ERROR, .error.code=(CODE), .error.argument=0}
+#define fail()
+#define nothing() (MaybeAst){AST_VALUE, .value=AST_NONE}
+#define just(AST) (MaybeAst){AST_VALUE, .value=(AST)}
+
+
+MaybeAst bind(MaybeAst ast, parser_function f, Input *input);
 
 Parser *make_parser(enum parser_kind kind, parser_function function);
 Parser *parser_init(Parser *self,
