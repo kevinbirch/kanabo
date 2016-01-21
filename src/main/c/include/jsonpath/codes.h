8eed2c261e2e46944e89e3318028b3870133aa70
kevin birch <kmb@percolate.com>
2016-01-20 20:13:15 -0500

    move parser codes into separate header

--- src/main/c/include/jsonpath/codes.h
+++ src/main/c/include/jsonpath/codes.h
@@ -35,48 +35,29 @@
  * [license]: http://www.opensource.org/licenses/ncsa
  */
 
-
 #pragma once
 
 
-#include <stdint.h>
+enum parser_result_code
+{
+    PARSER_SUCCESS = 0,
+    ERR_NULL_EXPRESSION,             // the expression argument given was NULL
+    ERR_ZERO_LENGTH,                 // expression length was 0
+    ERR_PARSER_OUT_OF_MEMORY,        // unable to allocate memory
+    ERR_NOT_JSONPATH,                // not a JSONPath expression
+    ERR_PREMATURE_END_OF_INPUT,      // premature end of input before expression was complete
+    ERR_UNEXPECTED_VALUE,            // expected one character but found another
+    ERR_EMPTY_PREDICATE,             // a predicate is empty
+    ERR_UNBALANCED_PRED_DELIM,       // missing closing predicate delimiter `]' before end of step
+    ERR_UNSUPPORTED_PRED_TYPE,       // unsupported predicate found
+    ERR_EXTRA_JUNK_AFTER_PREDICATE,  // extra characters after valid predicate definition
+    ERR_EXPECTED_NAME_CHAR,          // expected a name character, but found something else instead
+    ERR_EXPECTED_NODE_TYPE_TEST,     // expected a node type test
+    ERR_EXPECTED_INTEGER,            // expected an integer
+    ERR_INVALID_NUMBER,              // invalid number
+    ERR_STEP_CANNOT_BE_ZERO,         // slice step value must be non-zero
+    ERR_NO_ALTERNATIVE,              // none of the alternatives matched the input
+};
 
-#include "jsonpath/input.h"
-#include "jsonpath/codes.h"
+typedef enum parser_result_code parser_result_code;
 
-typedef struct parser_s Parser;
-
-
-/* Non-terminal parsers */
-
-Parser *rule_parser(const char *name, Parser *expression);
-#define rule(PARSER) rule_parser(__func__, (PARSER))
-
-Parser *choice_parser(Parser *one, Parser *two, ...);
-#define choice(...) choice_parser(__VA_ARGS__, NULL)
-
-Parser *sequence_parser(Parser *one, Parser *two, ...);
-#define sequence(...) sequence_parser(__VA_ARGS__, NULL)
-
-Parser *option(Parser *optional);
-Parser *repetition(Parser *repeated);
-
-/* Terminal parsers */
-
-Parser *literal(const char *value);
-
-Parser *number(void);
-Parser *integer(void);
-Parser *signed_integer(void);
-Parser *non_zero_signed_integer(void);
-
-Parser *quoted_string(void);
-Parser *string(void);
-
-/* Destructor */
-
-void parser_free(Parser *value);
-
-/* Parser Execution */
-
-char *parser_status_message(parser_result_code code, uint8_t argument, Input *input);
