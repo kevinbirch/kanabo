55a8cd3163920d08306f5422d455f7c2aa79b29b
kevin birch <kmb@pobox.com>
2016-04-02 20:43:14 -0400

    don't need this

--- src/main/c/include/jsonpath/logging.h
+++ src/main/c/include/jsonpath/logging.h
@@ -47,4 +47,3 @@
 #define parser_trace(FORMAT, ...) log_trace(component_name, FORMAT, ##__VA_ARGS__)
 
 #define trace_string(FORMAT, VALUE, LENGTH, ...) log_string(LVL_TRACE, component_name, FORMAT, VALUE, LENGTH, ##__VA_ARGS__)
-#define debug_string(FORMAT, VALUE, LENGTH, ...) log_string(LVL_DEBUG, component_name, FORMAT, VALUE, LENGTH, ##__VA_ARGS__)
