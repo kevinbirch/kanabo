c93b55c8aabac9957109ad4c65f45d9e065aa882
kevin birch <kmb@pobox.com>
2016-04-17 17:49:05 -0400

    reorg of input header

--- src/main/c/include/parser/input.h
+++ src/main/c/include/parser/input.h
@@ -1,39 +1,3 @@
-/*
- * 金棒 (kanabō)
- * Copyright (c) 2012 Kevin Birch <kmb@pobox.com>.  All rights reserved.
- *
- * 金棒 is a tool to bludgeon YAML and JSON files from the shell: the strong
- * made stronger.
- *
- * For more information, consult the README file in the project root.
- *
- * Distributed under an [MIT-style][license] license.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal with
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * - Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimers.
- * - Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimers in the documentation and/or
- *   other materials provided with the distribution.
- * - Neither the names of the copyright holders, nor the names of the authors, nor
- *   the names of other contributors may be used to endorse or promote products
- *   derived from this Software without specific prior written permission.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE CONTRIBUTORS
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
- *
- * [license]: http://www.opensource.org/licenses/ncsa
- */
 
 #pragma once
 
@@ -54,7 +18,6 @@ struct postion_s
 
 typedef struct postion_s Postion;
 
-typedef struct input_s Input;
 typedef struct file_input_s FileInput;
 typedef struct buffer_input_s BufferInput;
 
@@ -65,7 +28,7 @@ FileInput   *make_file_input(const char *filename);
 BufferInput *make_buffer_input(const uint8_t *data, size_t length);
 
 
-// Input Destructor
+// Input Destructors
 
 void dispose_file_input(FileInput *self);
 void dispose_buffer_input(BufferInput *self);
@@ -78,77 +41,142 @@ void dispose_buffer_input(BufferInput *self);
 
 // Input Property API
 
-String *file_input_name(FileInput *self);
+String *file_name(FileInput *self);
+String *buffer_name(BufferInput *self);
 
-// Input Coercion API
+#define input_name(SELF) _Generic((SELF),                     \
+                                  FileInput *: file_name,     \
+                                  BufferInput *: buffer_name  \
+                                  )(input(SELF))
 
-Input *file_input_upcast(FileInput *self);
-Input *buffer_input_upcast(BufferInput *self);
 
-#define input(SELF) _Generic((SELF),                            \
-                             FileInput *: file_input_upcast,    \
-                             BufferInput *: buffer_input_upcast \
-                             )(SELF)
 // Input Postion API
 
-Postion input_position(const Input *self);
+Postion file_position(const FileInput *self);
+Postion buffer_position(const BufferInput *self);
 
-#define position(SELF) _Generic((SELF),                           \
-                                FileInput *: input_position,      \
-                                BufferInput *: input_position     \
-                                )(input(SELF))
-
-void    input_advance_to_end(Input *self);
-
-#define advance_to_end(SELF) _Generic((SELF),                           \
-                                      FileInput *: input_advance_to_end, \
-                                      BufferInput *: input_advance_to_end \
+#define input_position(SELF) _Generic((SELF),                        \
+                                      FileInput *: file_position,    \
+                                      BufferInput *: buffer_position \
                                       )(input(SELF))
+
+void file_advance_to_end(FileInput *self);
+void buffer_advance_to_end(BufferInput *self);
+
+#define input_advance_to_end(SELF) _Generic((SELF),                     \
+                                            FileInput *: file_advance_to_end, \
+                                            BufferInput *: buffer_advance_to_end \
+                                            )(input(SELF))
 
 
 // Input Mark API
 
-void input_set_mark(Input *self);
-#define set_mark(SELF) _Generic((SELF),                           \
-                                FileInput *: input_set_mark,      \
-                                BufferInput *: input_set_mark     \
-                                )(input(SELF))
+void file_set_mark(FileInput *self);
+void buffer_set_mark(BufferInput *self);
 
-void input_reset_to_mark(Input *self);
-#define reset_to_mark(SELF) _Generic((SELF),                            \
-                                     FileInput *: input_reset_to_mark,  \
-                                     BufferInput *: input_reset_to_mark \
-                                     )(input(SELF))
+#define input_set_mark(SELF) _Generic((SELF),                           \
+                                      FileInput *: file_set_mark,       \
+                                      BufferInput *: buffer_set_mark    \
+                                      )(input(SELF))
 
-void input_rewind(Input *self);
-#define rewind(SELF) _Generic((SELF),                             \
-                              FileInput *: input_rewind,          \
-                              BufferInput *: input_rewind         \
-                              )(input(SELF))
+void file_reset_to_mark(FileInput *self);
+void buffer_reset_to_mark(BufferInput *self);
+
+#define input_reset_to_mark(SELF) _Generic((SELF),                      \
+                                           FileInput *: file_reset_to_mark, \
+                                           BufferInput *: buffer_reset_to_mark \
+                                           )(input(SELF))
+
+void file_rewind(FileInput *self);
+void buffer_rewind(BufferInput *self);
+
+#define input_rewind(SELF) _Generic((SELF),                       \
+                                    FileInput *: file_rewind,     \
+                                    BufferInput *: buffer_rewind  \
+                                    )(input(SELF))
 
 
 // Input Consumption API
 
-uint8_t peek(Input *self);
+uint8_t file_peek(FileInput *self);
+uint8_t buffer_peek(BufferInput *self);
 
-void    file_input_skip_whitespace(FileInput *self);
-void    buffer_input_skip_whitespace(BufferInput *self);
+#define input_peek(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_peek,    \
+                                  BufferInput *: buffer_peek \
+                                  )(SELF)
 
-#define skip_whitespace(SELF) _Generic((SELF),                          \
-                                       FileInput *: file_input_skip_whitespace, \
-                                       BufferInput *: buffer_input_skip_whitespace \
-                                       )(SELF)
+void file_skip_whitespace(FileInput *self);
+void buffer_skip_whitespace(BufferInput *self);
 
-uint8_t  consume_one(Input *self);
-String  *consume_many(Input *self, size_t count);
-bool     consume_if(Input *self, const uint8_t *value, size_t length);
-void     push_back(Input *self);
+#define input_skip_whitespace(SELF) _Generic((SELF),                    \
+                                             FileInput *: file_skip_whitespace, \
+                                             BufferInput *: buffer_skip_whitespace \
+                                             )(SELF)
+
+uint8_t file_consume_one(FileInput *self);
+uint8_t buffer_consume_one(BufferInput *self);
+
+#define input_consume_one(SELF) _Generic((SELF),                     \
+                                  FileInput *: file_consumme_one,    \
+                                  BufferInput *: buffer_consumme_one \
+                                  )(SELF)
+
+String *file_consume_many(FileInput *self, size_t count);
+String *buffer_consume_many(BufferInput *self, size_t count);
+
+#define input_consume_many(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_consume_many,    \
+                                  BufferInput *: buffer_consume_many \
+                                  )(SELF)
+
+bool file_consume_if(FileInput *self, const uint8_t *value, size_t length);
+bool buffer_consume_if(BufferInput *self, const uint8_t *value, size_t length);
+
+#define input_consume_if(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_consume_if,    \
+                                  BufferInput *: buffer_consume_if \
+                                  )(SELF)
+
+void file_push_back(FileInput *self);
+void buffer_push_back(BufferInput *self);
+
+#define input_push_back(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_push_back,    \
+                                  BufferInput *: buffer_push_back \
+                                  )(SELF)
 
 
 // Input Query API
 
-bool   has_more(Input *self);
-size_t remaining(Input *self);
+bool file_has_more(FileInput *self);
+bool buffer_has_more(BufferInput *self);
 
-bool   looking_at(Input *self, const char *value);
-size_t find(Input *self, const char *value);
+#define input_has_more(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_has_more,    \
+                                  BufferInput *: buffer_has_more \
+                                  )(SELF)
+
+size_t file_remaining(FileInput *self);
+size_t buffer_remaining(BufferInput *self);
+
+#define input_remaining(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_remaining,    \
+                                  BufferInput *: buffer_remaining \
+                                  )(SELF)
+
+bool file_looking_at(FileInput *self, const char *value);
+bool buffer_looking_at(BufferInput *self, const char *value);
+
+#define input_looking_at(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_looking_at,    \
+                                  BufferInput *: buffer_looking_at \
+                                  )(SELF)
+
+size_t file_find(FileInput *self, const char *value);
+size_t buffer_find(BufferInput *self, const char *value);
+
+#define input_find(SELF) _Generic((SELF),                    \
+                                  FileInput *: file_find,    \
+                                  BufferInput *: buffer_find \
+                                  )(SELF)
