34b936e051bd3aa2d9b4eeb3885f17361551684d
kevin birch <kmb@pobox.com>
2016-04-17 21:47:30 -0400

    add more generic functions

--- src/main/c/include/str.h
+++ src/main/c/include/str.h
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include <string.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -29,7 +30,7 @@ void    string_free(String *self);
 
 // String Data API
 
-uint8_t string_get_char(const String *self, size_t index);
+uint8_t string_get(const String *self, size_t index);
 
 
 // String Equality API
@@ -56,7 +57,7 @@ bool string_contains(const String *self, uint8_t value);
 
 // String Attribute API
 
-size_t string_get_length(const String *self);
+size_t string_length(const String *self);
 
 
 // String Coercion API
@@ -81,14 +82,14 @@ void mstring_free(MutableString *self);
 
 // Mutable String Attribute API
 
-size_t mstring_get_length(const MutableString *self);
+size_t mstring_length(const MutableString *self);
 size_t mstring_get_capacity(const MutableString *self);
 bool   mstring_has_capacity(const MutableString *self, size_t length);
 
 
 // Mutable String Data API
 
-uint8_t  mstring_get_char(const MutableString *self, size_t index);
+uint8_t  mstring_get(const MutableString *self, size_t index);
 
 
 // Mutable String Equality API
@@ -146,21 +147,134 @@ void mstring_set_range(MutableString *self, size_t position, size_t length, cons
 
 // Generic API
 
-#define string_length(SELF) _Generic((SELF),                            \
-                                     String *: string_get_length,       \
-                                     const String *: string_get_length, \
-                                     MutableString *: mstring_get_length, \
-                                     const MutableString *: mstring_get_length \
-                                     )(SELF)
+#define strlen(SELF) _Generic((SELF),                                   \
+                              String *: string_length,                  \
+                              const String *: string_length,            \
+                              MutableString *: mstring_length,          \
+                              const MutableString *: mstring_length,    \
+                              char *: strlen,                           \
+                              const char *: strlen                      \
+                              )(SELF)
 #define stridx(SELF, INDEX) _Generic((SELF),                            \
                                      String *: string_get,              \
                                      const String *: string_get,        \
                                      MutableString *: mstring_get,      \
                                      const MutableString *: mstring_get \
                                      )(SELF, INDEX)
-#define string_as_c_string(SELF) _Generic((SELF),                       \
-                                          String *: string_as_c_str,    \
-                                          const String *: string_as_c_str, \
-                                          MutableString *: mstring_as_c_str, \
-                                          const MutableString *: mstring_as_c_str \
-                                          )(SELF)
+#define strequ(SELF, OTHER, ...) _Generic((SELF),                       \
+                                          String *: _Generic((OTHER),   \
+                                                             String *: string_equals, \
+                                                             const String *: string_equals, \
+                                                             char *: string_equals_c_string, \
+                                                             const char *: string_equals_c_string, \
+                                                             uint8_t *: string_equals_bytestring, \
+                                                             const uint8_t *: string_equals_bytestring),         \
+                                          const String *: _Generic((OTHER),   \
+                                                                   String *: string_equals, \
+                                                                   const String *: string_equals, \
+                                                                   char *: string_equals_c_string, \
+                                                                   const char *: string_equals_c_string, \
+                                                                   uint8_t *: string_equals_bytestring, \
+                                                                   const uint8_t *: string_equals_bytestring), \
+                                          MutableString *: _Generic((OTHER),   \
+                                                                    MutableString *: mstring_equals, \
+                                                                    const MutableString *: mstring_equals, \
+                                                                    String *: mstring_equals_string, \
+                                                                    const String *: mstring_equals_string, \
+                                                                    char *: mstring_equals_c_string, \
+                                                                    const char *: mstring_equals_c_string, \
+                                                                    uint8_t *: mstring_equals_bytestring, \
+                                                                    const uint8_t *: mstring_equals_bytestring), \
+                                          const MutableString *: _Generic((OTHER),   \
+                                                                          MutableString *: mstring_equals, \
+                                                                          const MutableString *: mstring_equals, \
+                                                                          String *: mstring_equals_string, \
+                                                                          const String *: mstring_equals_string, \
+                                                                          char *: mstring_equals_c_string, \
+                                                                          const char*: mstring_equals_c_string, \
+                                                                          uint8_t *: mstring_equals_bytestring, \
+                                                                          const uint8_t *: mstring_equals_bytestring) \
+                                          )(SELF, OTHER, ##__VA_ARGS__)
+#define striter(SELF, ITER, PARAM) _Generic((SELF),                     \
+                                            String *: string_iterate,   \
+                                            const String *: string_iterate, \
+                                            MutableString *: mstring_iterate, \
+                                            const MutableString *: mstring_iterate, \
+                                            )(SELF, ITER, PARAM)
+#define strbeg(SELF, TEST) _Generic((SELF),                             \
+                                    String *: _Generic((OTHER),         \
+                                                       String *: string_startswith, \
+                                                       const String *: string_startswith, \
+                                                       char *: string_startswith_c_string, \
+                                                       const char *: string_startswith_c_string), \
+\
+                                    const String *: _Generic((OTHER),   \
+                                                             String *: string_startswith, \
+                                                             const String *: string_startswith, \
+                                                             char *: string_startswith_c_string, \
+                                                             const char *: string_startswith_c_string), \
+                                    MutableString *: _Generic((OTHER),  \
+                                                              MutableString *: mstring_startswith, \
+                                                              const MutableString *: mstring_startswith, \
+                                                              String *: mstring_startswith_string, \
+                                                              const String *: mstring_startswith_string, \
+                                                              char *: mstring_startswith_c_string, \
+                                                              const char *: mstring_startswith_c_string), \
+\
+                                    const MutableString *: _Generic((OTHER), \
+                                                                    MutableString *: mstring_startswith, \
+                                                                    const MutableString *: mstring_startswith, \
+                                                                    String *: mstring_startswith_string, \
+                                                                    const String *: mstring_startswith_string, \
+                                                                    char *: mstring_startswith_c_string, \
+                                                                    const char*: mstring_startswith_c_string, \
+ing) \
+                                    )(SELF, TEST)
+#define strend(SELF, TEST) _Generic((SELF),                             \
+                                    String *: _Generic((OTHER),         \
+                                                       String *: string_endswith, \
+                                                       const String *: string_endswith, \
+                                                       char *: string_endswith_c_string, \
+                                                       const char *: string_endswith_c_string), \
+\
+                                    const String *: _Generic((OTHER),   \
+                                                             String *: string_endswith, \
+                                                             const String *: string_endswith, \
+                                                             char *: string_endswith_c_string, \
+                                                             const char *: string_endswith_c_string), \
+                                    MutableString *: _Generic((OTHER),  \
+                                                              MutableString *: mstring_endswith, \
+                                                              const MutableString *: mstring_endswith, \
+                                                              String *: mstring_endswith_string, \
+                                                              const String *: mstring_endswith_string, \
+                                                              char *: mstring_endswith_c_string, \
+                                                              const char *: mstring_endswith_c_string), \
+\
+                                    const MutableString *: _Generic((OTHER), \
+                                                                    MutableString *: mstring_endswith, \
+                                                                    const MutableString *: mstring_endswith, \
+                                                                    String *: mstring_endswith_string, \
+                                                                    const String *: mstring_endswith_string, \
+                                                                    char *: mstring_endswith_c_string, \
+                                                                    const char*: mstring_endswith_c_string, \
+ing) \
+                                    )(SELF, TEST)
+
+#define strcast(SELF) _Generic((SELF),                                  \
+                               String *: string_as_c_str,               \
+                               const String *: string_as_c_str,         \
+                               MutableString *: mstring_as_c_str,       \
+                               const MutableString *: mstring_as_c_str  \
+                               )(SELF)
+#define strclone(SELF) _Generic((SELF),                              \
+                                String *: string_clone,              \
+                                const String *: string_clone,        \
+                                MutableString *: mstring_clone,      \
+                                const MutableString *: mstring_clone \
+                               )(SELF)
+#define strdisp(SELF) _Generic((SELF),                                 \
+                               String *: string_dispose,               \
+                               const String *: string_dispose,         \
+                               MutableString *: mstring_dispose,       \
+                               const MutableString *: mstring_dispose  \
+                               )(SELF)
