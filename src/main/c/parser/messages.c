ae1e68bd4703bbacf35a2246bb8d00d31e44310a
kevin birch <kmb@pobox.com>
2017-11-14 01:04:08 -0500

    add new parser errors

--- src/main/c/parser/messages.c
+++ src/main/c/parser/messages.c
@@ -4,7 +4,9 @@ static const char * const ERRORS[] =
 {
     [PREMATURE_END_OF_INPUT] = "premature end of input",
     [UNSUPPORTED_CONTROL_CHARACTER] = "unsupported control character",
-    [UNSUPPORTED_ESCAPE_SEQUENCE] = "unsupported escape sequence"
+    [UNSUPPORTED_ESCAPE_SEQUENCE] = "unsupported escape sequence",
+    [EXPECTED_QUALIFIED_STEP_PRODUCTION] = "expected step definition",
+    [EXPECTED_PREDICATE_EXPRESSION_PRODUCTION] = "expected predicate definition",
 };
 
 const char *parser_strerror(ParserErrorCode code)
