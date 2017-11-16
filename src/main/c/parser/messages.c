9f8c98e3c2cfb2c75a3c61d6778655eccfa605ec
kevin birch <kmb@percolate.com>
2017-11-15 22:20:42 -0500

    additional messages

--- src/main/c/parser/messages.c
+++ src/main/c/parser/messages.c
@@ -1,12 +1,14 @@
-#include "parser/errors.h"
+#include "parser.h"
 
 static const char * const ERRORS[] =
 {
     [PREMATURE_END_OF_INPUT] = "premature end of input",
     [UNSUPPORTED_CONTROL_CHARACTER] = "unsupported control character",
     [UNSUPPORTED_ESCAPE_SEQUENCE] = "unsupported escape sequence",
+    [EMPTY_INPUT] = "no expression provided",
     [EXPECTED_QUALIFIED_STEP_PRODUCTION] = "expected step definition",
     [EXPECTED_PREDICATE_EXPRESSION_PRODUCTION] = "expected predicate definition",
+    [EXPECTED_STEP_PRODUCTION] = "expected step selector or transformer definition",
 };
 
 const char *parser_strerror(ParserErrorCode code)
