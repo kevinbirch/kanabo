508d027347550b1a93a75660c0987fffd845934a
kevin birch <kmb@percolate.com>
2017-11-15 21:57:46 -0500

    remove some unneeded files

--- src/main/c/include/parser/recognizer.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "jsonpath/model.h"
-#include "parser/parse.h"
-
-JsonPath recognize(Parser *parser);
--- src/main/c/parser/read.c
+++
@@ -1,6 +0,0 @@
-#include "jsonpath.h"
-
-MaybeJsonPath read_path(const char *expression)
-{
-    return (MaybeJsonPath){.tag=NOTHING};
-}
